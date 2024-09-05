5e6b6bf84fd7e94aecefb71533ff4fcc227b6d3f
moojig12 <moojig5152@gmail.com>
2024-09-05 16:21:35 +0000

    later

---
+++ incl/minirt.h
@@ -0,0 +1,8 @@
+#ifndef MINIRT_H
+# define MINIRT_H
+
+#include <unistd.h>
+#include <math.h>
+#include "mlx.h"
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -2,5 +2,11 @@
 
 int	main(void)
 {
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 800, 600, "minirt");
+	mlx_hook(mlx_win, 0, 0, NULL, NULL);
 	return (0);
 }
